0b560201f7868594c7bf49ef281f64a1f08c5f78
chad-scanlon <59572157+chad-scanlon@users.noreply.github.com>
2020-09-25 23:26:10 -0400

    tests passing

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -11,7 +11,7 @@ class Node:
 
     def set_next(self, new_next):
         self.next_node = new_next
-
+    # makes this node's next_node pointer to the node passed in
 class LinkedList:
     def __init__(self):
         self.head = None
@@ -27,16 +27,30 @@ class LinkedList:
     def contains(self, value):
         if not self.head:
             return False
-
+    # is empty to set the value as the head
         current = self.head
 
         while current:
             if current.get_value() == value:
                 return True
-
+    # check the value, return out if it is that value
             current = current.get_next()
-
+    # move onto the next node 
+    # loop runs again to check the value, which isn't there so return out of it
         return False
 
     def reverse_list(self, node, prev):
-        pass
+        if node == None:
+            self.head = prev
+            # is empty set the head attribute
+        else:
+            next_node = node.next_node
+            # add a node to list 
+            node.next_node = prev
+            # move the pointer of the next node to become the previous node
+            prev = node
+            # prev becomes the head
+            node = next_node
+            # node passed in becomes the next node
+            self.reverse_list(node, prev)
+            
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,69 @@
+# from doubly_linked_list import DoublyLinkedList
+
+
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.storage = [None] * capacity
+        self.capacity = capacity
+        self.newest = 0
 
-    def append(self, item):
-        pass
+# make newest zero index
+# use a length method
+
+    def append(self, item):      
+        if self.newest == (len(self.storage)):
+            self.newest = 0
+            self.storage[self.newest] = item
+            self.newest += 1
+        else:
+            self.storage[self.newest] = item
+            self.newest += 1
 
     def get(self):
-        pass
+        
+        # list comprehension?
+        return [item for item in self.storage if item is not None]
+
+    def __len__(self):
+        return len(self.storage)
+
+
+# class RingBuffer:
+#     def __init__(self, capacity):
+#         self.storage = DoublyLinkedList()
+#         self.capacity = capacity
+#         self.newest = None
+
+#     def append(self, item):
+#         if len(self.storage) == 0:
+#             # check for empty list
+#             self.storage.add_to_head(item)
+#             self.newest = self.storage.head
+#             # add to list as head          
+#         elif len(self.storage) < self.capacity:
+#             self.storage.add_to_tail(item)
+#             # add to tail if not empty
+#         elif len(self.storage) == self.capacity:
+
+#             if self.current.next:
+#                 self.current.value = item
+#                 self.current = self.current.next
+            
+#             else: 
+#                 self.current.value = item
+
+#                 self.current = self.storage.head
+#     # while loop
+#     # append new value
+#     def get(self):
+#         contents = []
+
+#         node = self.storage.head
+
+#         while(node):
+#             contents.append(node.value)
+#             node = node.next
+#         return contents
+
+
+
